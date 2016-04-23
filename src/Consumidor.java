15e56ee943fe9bb9e280a7e3fb062bec463dae9e
moreirahelon <moreirahelon@gmail.com>
2016-04-23 14:59:58 -0300

    Solução completa do problema

--- src/Consumidor.java
+++ src/Consumidor.java
@@ -1,4 +1,31 @@
+public class Consumidor extends Thread {
+    private int idConsumidor;
+    private Buffer pilha;
+    private int totalConsumir;
+ 
+    public Consumidor(int id, Buffer p, int totalConsumir) {
+        idConsumidor = id;
+        pilha = p;
+        this.totalConsumir = totalConsumir;
+    }
+ 
+    public void run() {
+        for (int i = 0; i < totalConsumir; i++) {
+            pilha.get(idConsumidor);
+        }
+        System.out.println("Consumidor #" + idConsumidor + " concluido!");
+    }
 
-public class Consumidor {
-
+public static void main(String[] args) {
+    Buffer bufferCompartilhado = new Buffer();
+    Produtor produtor1 = new Produtor(1, bufferCompartilhado, 5);
+    Produtor produtor2 = new Produtor(2, bufferCompartilhado, 5);
+    Consumidor consumidor1 = new Consumidor(1, bufferCompartilhado, 2);
+    Consumidor consumidor2 = new Consumidor(2, bufferCompartilhado, 8);
+ 
+    produtor1.start();
+    consumidor1.start();
+    produtor2.start();
+    consumidor2.start();
+}
 }
--- src/Produtor.java
+++ src/Produtor.java
@@ -1,4 +1,31 @@
-
-public class Produtor {
-
+public class Produtor extends Thread {
+    private int idProdutor;
+    private Buffer pilha;
+    private int producaoTotal;
+ 
+    public Produtor(int id, Buffer p, int producaoTotal) {
+        idProdutor = id;
+        pilha = p;
+        this.producaoTotal = producaoTotal;
+    }
+ 
+    public void run() {
+        for (int i = 0; i < producaoTotal; i++) {
+            pilha.set(idProdutor, i);
+        }
+        System.out.println("Produtor #" + idProdutor + " concluido!");
+    }
+    
+    public static void main(String[] args) {
+        Buffer bufferCompartilhado = new Buffer();
+        Produtor produtor1 = new Produtor(1, bufferCompartilhado, 5);
+        Produtor produtor2 = new Produtor(2, bufferCompartilhado, 5);
+        Consumidor consumidor1 = new Consumidor(1, bufferCompartilhado, 2);
+        Consumidor consumidor2 = new Consumidor(2, bufferCompartilhado, 8);
+     
+        produtor1.start();
+        consumidor1.start();
+        produtor2.start();
+        consumidor2.start();
+    }
 }
